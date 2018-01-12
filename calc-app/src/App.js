8365fc12a123a4a21a47ee87e38b9f96de1874a1
JCamp-12 <username@users.noreply.github.com>
2018-01-12 18:58:30 -0500

    how far in little less than an hour, needs math in it

--- calc-app/src/App.js
+++ calc-app/src/App.js
@@ -3,16 +3,79 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    displayValue: '0'
+  }
+
+  clearDisplay() {
+    this.setState({
+      displayValue: '0'
+    })
+  }
+
+  inputDigit(digit){
+    const { displayValue } = this.state
+  
+
+  this.setState({
+    displayValue: displayValue === '0' ? String(digit) : displayValue + digit
+  })
+  }
+
   render() {
+    const { displayValue } = this.state
+
     return (
       <div className="App">
         <header className="App-header">
           
-          <h1 className="App-title">First Calc</h1>
+          <h1 className="App-title"></h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <div>
+            <div className="calc">
+              <div>
+              <div>
+                  
+              </div>
+                <div>
+                {displayValue}
+                </div>
+                <div>
+                  <button className="calc-key 0" onClick={() => this.inputDigit(0)}>0</button>
+                  <button className="calc-key .">. </button>
+                  <button className="calc-key 1" onClick={() => this.inputDigit(1)}>1 </button>
+                  <button className="calc-key +">+</button>
+                </div>
+                <div>
+                  <button className="calc-key 2" onClick={() => this.inputDigit(2)}>2 </button>
+                  <button className="calc-key 3" onClick={() => this.inputDigit(3)}>3 </button>
+                  <button className="calc-key 4" onClick={() => this.inputDigit(4)}>4 </button>
+                  <button className="calc-key -">-</button>
+                </div>
+                <div>
+                  <button className="calc-key 5" onClick={() => this.inputDigit(5)}>5 </button>
+                  <button className="calc-key 6" onClick={() => this.inputDigit(6)}>6 </button>
+                  <button className="calc-key 7" onClick={() => this.inputDigit(7)}>7 </button>
+                  <button className="calc-key *">*</button>
+                </div>
+                <div>
+                  <button className="calc-key 8" onClick={() => this.inputDigit(8)}>8 </button>
+                  <button className="calc-key 9" onClick={() => this.inputDigit(9)}>9 </button>
+                  <button className="calc-key /">/</button>
+                  <button className="calc-key %">%</button>
+                </div>
+                <div>
+                <button className="calc-key pos/neg">+/- </button>
+                <button className="calc-key ^">^</button>
+                <button className="calc-key ac" onClick={() => this.clearDisplay()}>ac</button>
+                  <button className="calc-key =">=</button>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
       </div>
     );
   }
